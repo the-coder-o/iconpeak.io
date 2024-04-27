e1b27614d7992c5b857270eef8e74ed2107a6c92
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-04-27 22:57:18 +0500

    finished categories sections responsive

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -269,56 +269,56 @@ const Home = () => {
           </div>
         </div>
       </div>
-      {/*<div className={'container'}>*/}
-      {/*  <div className={'mb-[100px] flex items-center justify-between'}>*/}
-      {/*    <div>*/}
-      {/*      <Image src={'https://img.fortawesome.com/1ce05b4b/open-source-illustration.svg'} alt={''} width={100} height={100} className={'!w-full'} />*/}
-      {/*    </div>*/}
-      {/*    <div className={'flex flex-col gap-5'}>*/}
-      {/*      <h3 className={'text-[17px] font-[600] tracking-[5px] text-[#B197FC]'}>PROUDLY OPEN SOURCE</h3>*/}
-      {/*      <h2 className={'text-[40px] font-[500] text-[#183153]'}>Everyone gets more awesome.</h2>*/}
-      {/*      <p className={'w-[528px] text-[16px]'}>We know the pain of wrangling icons on the internet. That&paos;s why in 2012, we created the first version of our open-source icons and toolkit. And with the help of our subscription plan Font Awesome Pro, we&apos;ve built a lean icon-obsessed team who keep cranking out more icons, more styles, and more services for everyone.</p>*/}
-      {/*      <Link href={'/'} className={'shadow-custom-box mt-[50px] flex items-center justify-center gap-4 rounded-[0.65rem] border-[0.125rem] border-[#183153] bg-[#B197FC] px-[65px] py-[12px] text-[18px] font-medium text-[#183153]'}>*/}
-      {/*        <i className="fa-solid fa-icons"></i>*/}
-      {/*        All icons for free*/}
-      {/*      </Link>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*  <div className={'grid grid-cols-3 items-center gap-[30px]'}>*/}
-      {/*    <div className={'relative rounded-[16px] bg-[#F0F1F3] p-5'}>*/}
-      {/*      <span className={'absolute -top-3 right-8 rounded-lg bg-[#183153] px-3 py-1 text-xs font-medium text-white'}>SOON !</span>*/}
-      {/*      <div className={'flex flex-col gap-4'}>*/}
-      {/*        <i className="fa-solid fa-handshake text-center text-5xl text-[#6741D9]"></i>*/}
-      {/*        <h3 className={'text-xl text-[#183153]'}>Iconpeak Free: Always Free</h3>*/}
-      {/*        <p className={'text-[#183153]'}>From the beginning, Font Awesome has offered free and open source icons. And since we&apos;re creator owned and operated, we&apos;ll make sure Font Awesome Free stays that way.</p>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*    <div className={'relative rounded-[16px] bg-[#F0F1F3] p-5'}>*/}
-      {/*      <span className={'absolute -top-3 right-8 rounded-lg bg-[#183153] px-3 py-1 text-xs font-medium text-white'}>SOON !</span>*/}
-      {/*      <div className={'flex flex-col gap-4'}>*/}
-      {/*        <i className="fa-solid fa-shield-heart text-center text-5xl text-[#6741D9]"></i>*/}
-      {/*        <h3 className={'text-xl text-[#183153]'}>No Shenanigans</h3>*/}
-      {/*        <p className={'text-[#183153]'}>We have more icons, services, and support in Pro. If you want them, you subscribe. No shenanigans. We won&apos;t sell your data, because we don&apos;t want to live in that world, and we don&apos;t want to run that company.</p>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*    <div className={'relative rounded-[16px] bg-[#F0F1F3] p-5'}>*/}
-      {/*      <span className={'absolute -top-3 right-8 rounded-lg bg-[#183153] px-3 py-1 text-xs font-medium text-white'}>SOON !</span>*/}
-      {/*      <div className={'flex flex-col gap-4'}>*/}
-      {/*        <i className="fa-solid fa-rocket text-center text-5xl text-[#6741D9]"></i>*/}
-      {/*        <h3 className={'text-xl text-[#183153]'}>Support Open Source</h3>*/}
-      {/*        <p className={'text-[#183153]'}>Give back to the open-source community by subscribing to Font Awesome Pro. In fact, Pro subscribers helped grow Font Awesome Free icons by over 250% since version 4.</p>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*  <div className={'mb-[100px] mt-[50px] flex items-center justify-center'}>*/}
-      {/*    <div className={'flex items-center gap-3'}>*/}
-      {/*      <i className="fa-brands fa-github text-2xl"></i>*/}
-      {/*      <Link href={'https://github.com/the-coder-o/iconpeak.io'} target={'_blank'} className={'text-xl font-[500] underline decoration-dotted hover:text-[#6741D9]'}>*/}
-      {/*        Iconpeak.io is one of the top open source projects on GitHub.*/}
-      {/*      </Link>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+      <div className={'container'}>
+        <div className={'mb-[100px] flex items-center justify-between max-lg:flex-col max-lg:gap-10'}>
+          <div>
+            <Image src={'https://img.fortawesome.com/1ce05b4b/open-source-illustration.svg'} alt={''} width={100} height={100} className={'!w-full'} />
+          </div>
+          <div className={'flex flex-col gap-5 max-lg:gap-2'}>
+            <h3 className={'text-[17px] font-[600] tracking-[5px] text-[#B197FC]'}>PROUDLY OPEN SOURCE</h3>
+            <h2 className={'text-[40px] font-[500] text-[#183153] max-md:text-[25px]'}>Everyone gets more awesome.</h2>
+            <p className={'w-[528px] text-[16px] max-lg:w-full'}>We know the pain of wrangling icons on the internet. That&paos;s why in 2012, we created the first version of our open-source icons and toolkit. And with the help of our subscription plan Font Awesome Pro, we&apos;ve built a lean icon-obsessed team who keep cranking out more icons, more styles, and more services for everyone.</p>
+            <Link href={'/'} className={'shadow-custom-box mt-[50px] flex items-center justify-center gap-4 rounded-[0.65rem] border-[0.125rem] border-[#183153] bg-[#B197FC] px-[65px] py-[12px] text-[18px] font-medium text-[#183153] max-md:mt-[25px]'}>
+              <i className="fa-solid fa-icons"></i>
+              All icons for free
+            </Link>
+          </div>
+        </div>
+        <div className={'grid grid-cols-3 items-center gap-[30px] max-md:flex max-md:flex-col'}>
+          <div className={'relative rounded-[16px] bg-[#F0F1F3] p-5'}>
+            <span className={'absolute -top-3 right-8 rounded-lg bg-[#183153] px-3 py-1 text-xs font-medium text-white'}>SOON !</span>
+            <div className={'flex flex-col gap-4'}>
+              <i className="fa-solid fa-handshake text-center text-5xl text-[#6741D9] max-md:text-[35px]"></i>
+              <h3 className={'text-xl text-[#183153]'}>Iconpeak Free: Always Free</h3>
+              <p className={'text-[#183153]'}>From the beginning, Font Awesome has offered free and open source icons. And since we&apos;re creator owned and operated, we&apos;ll make sure Font Awesome Free stays that way.</p>
+            </div>
+          </div>
+          <div className={'relative rounded-[16px] bg-[#F0F1F3] p-5'}>
+            <span className={'absolute -top-3 right-8 rounded-lg bg-[#183153] px-3 py-1 text-xs font-medium text-white'}>SOON !</span>
+            <div className={'flex flex-col gap-4'}>
+              <i className="fa-solid fa-shield-heart text-center text-5xl text-[#6741D9] max-md:text-[35px]"></i>
+              <h3 className={'text-xl text-[#183153]'}>No Shenanigans</h3>
+              <p className={'text-[#183153]'}>We have more icons, services, and support in Pro. If you want them, you subscribe. No shenanigans. We won&apos;t sell your data, because we don&apos;t want to live in that world, and we don&apos;t want to run that company.</p>
+            </div>
+          </div>
+          <div className={'relative rounded-[16px] bg-[#F0F1F3] p-5'}>
+            <span className={'absolute -top-3 right-8 rounded-lg bg-[#183153] px-3 py-1 text-xs font-medium text-white'}>SOON !</span>
+            <div className={'flex flex-col gap-4'}>
+              <i className="fa-solid fa-rocket text-center text-5xl text-[#6741D9] max-md:text-[35px]"></i>
+              <h3 className={'text-xl text-[#183153]'}>Support Open Source</h3>
+              <p className={'text-[#183153]'}>Give back to the open-source community by subscribing to Font Awesome Pro. In fact, Pro subscribers helped grow Font Awesome Free icons by over 250% since version 4.</p>
+            </div>
+          </div>
+        </div>
+        <div className={'mb-[100px] mt-[50px] flex items-center justify-center max-md:mb-[50px]'}>
+          <div className={'flex items-center gap-3'}>
+            <i className="fa-brands fa-github text-2xl max-md:text-xl"></i>
+            <Link href={'https://github.com/the-coder-o/iconpeak.io'} target={'_blank'} className={'text-xl font-[500] underline decoration-dotted hover:text-[#6741D9] max-md:text-sm'}>
+              Iconpeak.io is one of the top open source projects on GitHub.
+            </Link>
+          </div>
+        </div>
+      </div>
       {/*<div className={'bg-[#183153] p-10'}>*/}
       {/*  <div className={'container'}>*/}
       {/*    <div>*/}
